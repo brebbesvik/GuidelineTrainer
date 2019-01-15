5c09289b5da0f6d536e3a189477bf3b5c7bd08e5
brebbesvik <brebbesvik@gmail.com>
2019-01-15 17:31:52 +0100

    Cleaned up the code

--- src/Reducers/CounterReducer.js
+++ src/Reducers/CounterReducer.js
@@ -1,25 +1,10 @@
 import * as Actions from '../Actions/ActionTypes';
-import QuestionMocks from '../MockingData/QuestionMocks';
-//import QuizDAO from '../DAO/QuizDAO';
 import QuestionDAO from '../DAO/QuestionDAO';
 import Quiz from '../Model/Quiz';
 
 const quiz = new Quiz();
 quiz.setCategory("Asthma");
 quiz.addQuestions(QuestionDAO.getQuestions("Asthma", "Examination", 1));
-
-/*const initialState = {
-  count: 5,
-  problem: quiz.getQuestion().getNarrative(),
-  alternatives: ["LOL", "ROFL", "LMAO"],
-  correctAlternative: quiz.getQuestion().getAnswerKey(),
-  questionNumber: quiz.getQuestionNumber(),
-  numberOfQuestions: quiz.getNumberOfQuestions(),
-  answerKeyExplanation: quiz.getQuestion().getAnswerExplanation()
-};
-*/
-const questionMocks = new QuestionMocks();
-questionMocks.generate();
 
 const initialState = {
     count: 5,
