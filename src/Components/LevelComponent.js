7bc36519b9ab331ea185f1e8a16fec1f227efdb2
brebbesvik <brebbesvik@gmail.com>
2019-01-15 18:24:18 +0100

    Cleaned up imports

--- src/Components/LevelComponent.js
+++ src/Components/LevelComponent.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import {Button, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {Navigation} from "react-native-navigation";
 
 const mapStateToProps = (state) => ({
