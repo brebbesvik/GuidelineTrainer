8681ddbfdb4ef6449418f225c3a2f29fab6aede1
brebbesvik <brebbesvik@gmail.com>
2019-01-31 14:11:26 +0100

    Opacity on modals

--- src/Components/AnswerKeyComponent.js
+++ src/Components/AnswerKeyComponent.js
@@ -88,6 +88,7 @@ const styles = StyleSheet.create({
     flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#000000AA"
     },
     modalView: {
         height: '60%',
--- src/Components/ProgressComponent.js
+++ src/Components/ProgressComponent.js
@@ -85,6 +85,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#000000AA"
     },
     modalView: {
         height: '90%',
