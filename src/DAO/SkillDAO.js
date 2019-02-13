10f86ab16e02aae32df20a80ae51c657f0e34733
brebbesvik <brebbesvik@gmail.com>
2019-02-13 18:54:04 +0100

    Removed unused code. Should probably refactor to remove this class.

--- src/DAO/SkillDAO.js
+++ src/DAO/SkillDAO.js
@@ -1,11 +1,6 @@
 import {AsyncStorage} from 'react-native';
-import Skill from '../Model/Skill';
 class SkillDAO {
     constructor() {
-        this._skills = [];
-    }
-    getSkills() {
-        return this._skills;
     }
     static saveScores(category, skills) {
         let dict = {};
@@ -19,28 +14,8 @@ class SkillDAO {
             console.log("Error storing " + error);
         }
     }
-    static saveScore(category, skill) {
-        try {
-            AsyncStorage.setItem(category + skill.getDiscipline(), skill.getScore().toString());
-        }catch(error) {
-            console.log("Error storing " + error);
-        }
-    }
-
-
     static getScore (category, discipline) {
         return AsyncStorage.getItem(category + discipline);
-        /*.then((value)=>{
-                console.log(value);
-                score.setScore(parseFloat(value));
-                this._skills.push(score);
-            });
-            console.log("1 " + score.getScore());
-        }catch(error) {
-            console.log("Error retrieving: " + error);
-        }
-        console.log("2 " + score.getScore());
-        return score;*/
     }
 
 }
